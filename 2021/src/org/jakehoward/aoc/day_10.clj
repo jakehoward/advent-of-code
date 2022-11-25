512d6dbe26ad89c891afb569b89f4cd32a7567a3
Jake Howard <jake@jakehoward.org>
2022-11-25 20:29:58 +0000

    Day 10 understood

--- 2021/src/org/jakehoward/aoc/day_10.clj
+++ 2021/src/org/jakehoward/aoc/day_10.clj
@@ -2,10 +2,8 @@
   (:require [org.jakehoward.aoc.utils :as utils]
             [clojure.string :as string]))
 
-(def data-lines (-> (utils/get-input 10)
-                    (utils/lines)))
-
-(def data (map #(string/split % #"") data-lines))
+(def data (-> (utils/get-input 10)
+              (utils/lines)))
 
 (def matching-pairs {\{ \}, \[ \], \( \), \< \>})
 (def reverse-matching-pairs (->> matching-pairs
@@ -13,30 +11,24 @@
                                  (map vec)
                                  (into {})))
 
-(defn update-tally [tally char]
-  (let [updated-tally (if (contains? matching-pairs char)
-                       (update tally char inc)
-                       (update tally (reverse-matching-pairs char) dec))]
-    (println "char:" char "tally:" updated-tally)
-    updated-tally))
-
-(defn get-offending-char [tally]
-  (->> tally
-       (some #(when (< (second %) 0) %))
-       first
-       matching-pairs))
+(defn is-opening? [char]
+  (boolean (matching-pairs char)))
 
 (defn first-illegal-character [chars]
-  (loop [tally            {\{ 0, \[ 0, \( 0, \< 0}
-         remaining-chars  (seq chars)]
-
-    (let [offending-char (get-offending-char tally)
-          char (first remaining-chars)]
-
+  (loop [remaining-chars (seq chars)
+         char-stack      []]
+    (let [char (first remaining-chars)]
       (cond
-        (some? offending-char)  offending-char
-        (not (some? char))      nil
-        :else                   (recur (update-tally tally char) (rest remaining-chars))))))
+        (not (some? char))   nil
+        ;; (not (#{\{ \}, \[ \], \( \), \< \>} char)) (throw (Exception. (str "Err, no:" char)))
+
+        (is-opening? char)   (recur (rest remaining-chars) (conj char-stack char))
+
+        ;; we assume...
+        :is-closing          (if (= (reverse-matching-pairs char)
+                                    (peek char-stack))
+                               (recur (rest remaining-chars) (pop char-stack))
+                               char)))))
 
 (def points {\) 3 \] 57 \} 1197 \> 25137})
 
@@ -61,14 +53,22 @@
                     "<{([{{}}[<[[[<>{}]]]>[]]"]]
     (map first-illegal-character test-input))
 
+  ;; it's a stack where the closing
+  ;; character can pop an item off the
+  ;; stack, if it's not closing then
+  ;; it's illegal
   (let [examples ["{([(<{}[<>[]}>{[]{[(<()>" ;; Expected ], but found } instead.
                   "[[<[([]))<([[{}[[()]]]"   ;; Expected ], but found ) instead.
                   "[{[{({}]{}}([{[{{{}}([]"  ;; Expected ), but found ] instead.
                   "[<(<(<(<{}))><([]([]()"   ;; Expected >, but found ) instead.
                   "<{([([[(<>()){}]>(<<{{"   ;; Expected ], but found > instead.
                   ]]
-    (map first-illegal-character (take 1 examples)))
+    (map first-illegal-character
+         ;; (take 1 (drop 1 examples))
+         examples
+         ))
 
   (last data)
-  ;; => 126882
-  (play))
+  ;; => 341823
+  (play)
+  )
