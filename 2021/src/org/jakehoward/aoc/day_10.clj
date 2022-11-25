97414ca7749280f4043f5ad69893c7f0de4a03c6
Jake Howard <jake@jakehoward.org>
2022-11-25 20:52:27 +0000

    Day 10 pt 2

--- 2021/src/org/jakehoward/aoc/day_10.clj
+++ 2021/src/org/jakehoward/aoc/day_10.clj
@@ -30,6 +30,23 @@
                                (recur (rest remaining-chars) (pop char-stack))
                                char)))))
 
+(defn get-missing-chars [chars]
+  (loop [remaining-chars (seq chars)
+         char-stack      []]
+    (let [char (first remaining-chars)]
+      (cond
+        (not (some? char)) (reverse (map matching-pairs char-stack))
+        ;; (not (#{\{ \}, \[ \], \( \), \< \>} char)) (throw (Exception. (str "Err, no:" char)))
+
+        (is-opening? char)   (recur (rest remaining-chars) (conj char-stack char))
+
+        ;; we assume...
+        :is-closing          (if (= (reverse-matching-pairs char)
+                                    (peek char-stack))
+                               (recur (rest remaining-chars) (pop char-stack))
+                               (throw
+                                (Exception. "Don't call me with corrupted lines, dude")))))))
+
 (def points {\) 3 \] 57 \} 1197 \> 25137})
 
 (defn play []
@@ -37,7 +54,26 @@
         score (reduce + (map points illegal-chars))]
     score))
 
+(def points-pt2 {\) 1 \] 2 \} 3  \> 4})
+
+(defn score-pt2 [chars]
+  (reduce (fn [acc char] (+ (points-pt2 char) (* 5 acc))) 0 chars))
+
+(defn play-pt2 []
+  (let [incomplete-lines (filter #(nil? (first-illegal-character %)) data)
+        completions      (map get-missing-chars incomplete-lines)
+        scores           (map score-pt2 completions)
+        sorted-scores    (sort scores)
+        middle-score     (first (drop (int (/ (count sorted-scores) 2)) sorted-scores))]
+    middle-score))
+
 (comment
+  (let [test-input ["[({(<(())[]>[[{[]{<()<>>" ;; Complete by adding }}]])})].
+                    ]]
+    (map get-missing-chars test-input))
+  ;; 2801302861
+  (play-pt2)
+
   (let [test-input "{([(<{}[<>[]}>{[]{[(<()>"]
     (first-illegal-character test-input))
 
@@ -65,10 +101,8 @@
                   ]]
     (map first-illegal-character
          ;; (take 1 (drop 1 examples))
-         examples
-         ))
+         examples))
 
   (last data)
   ;; => 341823
-  (play)
-  )
+  (play))
