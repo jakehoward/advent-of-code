394d08a06cbed05a435d41e77bb75bca7fa90ef3
Jake Howard <jake@jakehoward.org>
2022-12-09 08:48:00 +0000

    Add template

---
+++ 2022/src/org/jakehoward/aoc/days/day_n.clj
@@ -0,0 +1,17 @@
+(ns org.jakehoward.aoc.days.day-n
+  (:require [org.jakehoward.aoc.utils :as utils]
+            [clojure.string :as str]))
+
+(defn parse-input [input]
+  input)
+
+(defn part-1 [input]
+  (let [xxx (parse-input input)
+        ans xxx]
+    ans))
+
+(comment
+  (part-1 example-input)
+  (part-1 (utils/get-input "< put day here>"))
+  (def example-input ""))
+
