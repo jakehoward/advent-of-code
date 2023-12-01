724a57649e69c6fd9b993bca828e455e41f0aee4
Jake Howard <jake@jakehowrd.org>
2023-12-01 13:33:06 +0000

    commit before re-write

--- 2023/src/aoc/day_01.clj
+++ 2023/src/aoc/day_01.clj
@@ -19,8 +19,8 @@
         ans       (sum cal-vals)]
     ans))
 
-(def num-strs  ["0"     "1"   "2"   "3"     "4"    "5"    "6"   "7"     "8"     "9"])
-(def num-words ["zero"  "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
+(def num-strs  ["0" "1"   "2"   "3"     "4"    "5"    "6"   "7"     "8"     "9"])
+(def num-words [    "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 
 (defn figure-it-out [line]
   (->> (concat num-strs num-words)
@@ -28,6 +28,34 @@
        (filter (fn [[s idx]] idx))
        (sort-by second)
        (map first)))
+(comment
+  (figure-it-out "sevenine")
+  (figure-it-out "7sevenine")
+  (figure-it-out ""))
+
+(defn figure-it-out-no-overlaps [line]
+  (loop [tokens []
+         s      line]
+    ;; if index-of any of the tokens is zero add to found and remove from s
+    (if (empty? s)
+      tokens
+
+      (let [found
+            (->> (concat num-strs num-words)
+                 (map (fn [token] [token (str/index-of s token)]))
+                 (filter (fn [[t idx]] (and idx (zero? idx))))
+                 first)]
+        (if found
+          (recur (conj tokens (first found)) (if (>= (count s) (count (first found)))
+                                               (.substring s (count (first found)))
+                                               ""))
+          (recur tokens (.substring s 1)))))))
+
+(comment
+  (figure-it-out-no-overlaps "sevenine")
+  (figure-it-out "7sevenine")
+  (figure-it-out ""))
+
 
 (comment
   (.indexOf num-strs "0")
@@ -39,16 +67,19 @@
         lnums       (map figure-it-out lines)
         fst-lst     (map (fn [nums] ((juxt first last) nums)) lnums)
         nums-2-sumz (map (fn [[a b]] (str (max (.indexOf num-strs a)
-                                             (.indexOf num-words a))
-                                        (max (.indexOf num-strs b)
-                                             (.indexOf num-words b)))) fst-lst)
-        ans (map #(Integer/parseInt %) nums-2-sumz) ;;nums-2-sumz ;; (sum (map #(Integer/parseInt %) nums-2-sumz))
+                                               (inc (.indexOf num-words a)))
+                                          (max (.indexOf num-strs b)
+                                               (inc (.indexOf num-words b))))) fst-lst)
+        ans (sum (map #(Integer/parseInt %) nums-2-sumz))
+        ans nums-2-sumz
+        ;; ans fst-lst
+        ;; ans lnums
         ]
     ans))
 
 (comment
-  (pt2 input) ;; 54498 (too low)
-  
+  (count (pt2 input));; 35739 (too low) ;; 54498 ;; 54498 ;; 54498 (too low)
+
   (let [input "two1nine
 eightwothree
 abcone2threexyz
@@ -56,31 +87,12 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen"]
-    (pt2 input)) ;; (29 83 13 24 42 14 76) ;; ("29" "83" "13" "24" "42" "14" "76")
-(Integer/parseInt "05")
+    (pt2 input))
+  ;; (29 83 13 24 42 14 76) ;; ("29" "83" "13" "24" "42" "14" "76")
+
   (pt1 input)
   (let [input "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"]
-    (pt1 input))
-
-  
-
-  (let [lines     (str/split-lines input)
-        int-strs  #{"1" "2" "3" "4" "5" "6" "7" "8" "9" "0"}
-        first-int (fn [line] (->> line str->cs (drop-while #(not (contains? int-strs %))) first))
-        last-int  (fn [line] (->> line str->cs reverse (drop-while #(not (contains? int-strs %))) first))
-        cal-vals  (->> lines
-                       (map #((juxt first-int last-int) %))
-                       (map #(map (fn [is] (Integer/parseInt is)) %)))
-        ans       (sum (map sum cal-vals))
-        ]
-    ans)
-  
-  
-  (let [int-strs  #{"1" "2" "3" "4" "5" "6" "7" "8" "9" "0"}
-        first-int (fn [line] (->> line str->cs (drop-while #(not (contains? int-strs %))) first))
-        last-int  (fn [line] (->> line str->cs reverse first-int))]
-    (map (fn [l] ((juxt first-int last-int) l)) ["asdf125sadf5asdf"]))
-  )
+    (pt1 input)))
