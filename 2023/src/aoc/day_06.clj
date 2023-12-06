8911260f3b6906ae79966340a3716317dda34088
Jake Howard <jake@jakehowrd.org>
2023-12-06 06:01:25 +0000

    Cleaned up

--- 2023/src/aoc/day_06.clj
+++ 2023/src/aoc/day_06.clj
@@ -1,18 +1,10 @@
-(ns aoc.day-06
-  (:require [aoc.utils :as u]
-            [clojure.string :as str]))
+(ns aoc.day-06)
 
-;; (def example (str/trim "
-;; Time:      7  15   30
-;; Distance:  9  40  200"))
 (def example [{:time 7 :distance 9}
               {:time 15 :distance 40}
               {:time 30 :distance 200}])
 
 (def example-pt2 {:time 71530 :distance 940200})
-
-;; (def input (u/get-input 6))
-
 
 (def input  [{:time 48 :distance 296}
              {:time 93 :distance 1928}
@@ -20,11 +12,6 @@
              {:time 95 :distance 1391}])
 
 (def input-pt2 {:time 48938595 :distance 296192812361391})
-;; (str/trim "
-;; Time:        48     93     85     95
-;; Distance:   296   1928   1236   1391")
-
-(def parse-input identity)
 
 (defn num-ways-to-win [{:keys [time distance]}]
   (let [ways         (range 0 time)
@@ -32,8 +19,7 @@
     (count winning-ways)))
 
 (defn pt1 [input]
-  (let [parsed (parse-input input)]
-    (reduce * 1 (map num-ways-to-win parsed))))
+  (reduce * 1 (map num-ways-to-win input)))
 
 (defn pt2 [input]
   (num-ways-to-win input))
