c8382633066066dcd8450e2918d4b062e8d84be5
Jake Howard <jake@jakehowrd.org>
2023-12-26 09:22:37 +0000

    Don't do lots of work again and again...

--- 2023/src/aoc/day_12.clj
+++ 2023/src/aoc/day_12.clj
@@ -1,5 +1,6 @@
 (ns aoc.day-12
   (:require [aoc.utils :as u]
+            [clojure.pprint :refer [pprint]]
             [clojure.string :as str]
             [clojure.math.combinatorics :as prof-b]))
 
@@ -89,44 +90,102 @@
                dots     [starting-dot-config]]
           (if (= 0 rem-dots)
             dots
-            (let [next-configs (vec (mapcat increment-all dots))]
+            (let [next-configs (vec (set (mapcat increment-all dots)))]
               (recur (dec rem-dots) next-configs))))]
-    (mapv (partial dot-config->template groups) dot-configs)))
+    (mapv (partial dot-config->template groups) (set dot-configs))))
 
 (comment
   (get-ways-to-make-groups ".??.??.###" [1 1 3]))
 
-(defn- way-matches-template [template way]
+(defn- way-matches-template? [template way]
   (assert (= (count template) (count way)) (str "Nope. T: " (count template) " W: " (count way)))
   (let [valid-pair? (fn [[template-char way-char]]
                       (if (= \? template-char) true (= way-char template-char)))]
     (every? valid-pair? (map vector template way))))
 
-(comment (way-matches-template "?.?#" "..##")
-         (way-matches-template "?.?#" "#.##")
-         (way-matches-template "?.?#" "#.#."))
+(comment (way-matches-template? "?.?#" "..##")
+         (way-matches-template? "?.?#" "#.##")
+         (way-matches-template? "?.?#" "#.#."))
 
-(defn num-ways-per-line [{:keys [template groups]}]
-  (let [ways-to-make-groups (get-ways-to-make-groups template groups)
-        valid-ways          (filterv (partial way-matches-template template) ways-to-make-groups)]
-    (count valid-ways)))
+(defn num-ways-per-line [{:keys [template groups] :as line}]
+  (let [start               (System/nanoTime)
+        ways-to-make-groups (get-ways-to-make-groups template groups)
+        valid-ways          (filterv (partial way-matches-template? template) ways-to-make-groups)
+        num-valid-ways      (count valid-ways)
+        end                 (System/nanoTime)
+        time-ms             (/ (- end start) 1e6)]
 
+    (when (> time-ms 1000)
+      (pprint {:time-ms time-ms
+               :line line
+               :num-ways-to-make-groups (count ways-to-make-groups)
+               :num-valid-ways num-valid-ways}))
+
+    num-valid-ways))
+
+(comment (num-ways-per-line {:template "??.??.###" :groups [1 1 3]})
+         (num-ways-per-line {:template ".??..??...?##." :groups [1 1 3]})
+         (time (num-ways-per-line {:template "??#????#..?.?.?", :groups [3 2 1 1 1]}))
+         (time (num-ways-per-line {:template "??#??..#??.", :groups [3 1]}))
+         (time (num-ways-per-line {:template ".?????.??.???.?#??", :groups [1 1 3]}))
+         )
+
+;; ways   (map num-ways-per-line (take 10 (drop 275 parsed)))
 (defn pt1 [input]
-  (let [parsed (parse-input input)
-        ways   (map og-ways-per-line parsed)
-        ans    (map count ways)]
-    (u/sum ans)))
-
-(defn pt2 [input]
+  (println "--- Pt1 ---")
   (let [parsed (parse-input input)
         ways   (map num-ways-per-line parsed)
+        ;; ways   (map num-ways-per-line (take 700 parsed))
         ans    (u/sum ways)]
     ans))
 
+;; (defn pt2 [input]
+  ;; (let [parsed (parse-input input)
+        ;; ways   (map num-ways-per-line (take 4 parsed))
+        ;; ans    (u/sum (map :num-valid-ways ways))]
+    ;; ans))
+
 (comment
-  (time (pt2 example))
-  (time (pt2 input))
   (time (pt1 example))
-  (time (pt1 input)) ;; 7792
+  (time (pt1 input))
+  ;; (time (pt2 example))
+  ;; (time (pt2 input))
+  ;; pt1 ans 7792
+
+  ;; Observations:
+  ;; - Techniques that just use the template or just use the groups to
+  ;;   enumerate options have pathalogical data that kill their performance
+  ;;   => need a solution that takes info of both into account
+
+
+  ;;
+  {:time-ms 2982.466057,
+ :line {:template ".?????.??.???.?#??", :groups [1 1 3]},
+ :num-ways-to-make-groups 364,
+ :num-valid-ways 76}
+{:time-ms 1298.929591,
+ :line {:template ".?.??.?.#?#?#.????..", :groups [1 5]},
+ :num-ways-to-make-groups 105,
+ :num-valid-ways 4}
+{:time-ms 11827.337391,
+ :line {:template ".....??###??..???.??", :groups [3 1]},
+ :num-ways-to-make-groups 136,
+ :num-valid-ways 6}
+{:time-ms 1194.717369,
+ :line {:template "??#??.???..?.?.???", :groups [2 2]},
+ :num-ways-to-make-groups 105,
+ :num-valid-ways 8}
+
+  ;;
+  {:time-ms 2836.041536,
+   :line {:template ".?????.??.???.?#??", :groups [1 1 3]}}
+  {:time-ms 1253.068307,
+   :line {:template ".?.??.?.#?#?#.????..", :groups [1 5]}}
+  {:time-ms 12040.617259,
+   :line {:template ".....??###??..???.??", :groups [3 1]}}
+  {:time-ms 1191.902384,
+   :line {:template "??#??.???..?.?.???", :groups [2 2]}}
+  {:time-ms 3986.236152,
+   :line {:template "...??.?.?#.?????.", :groups [1 1]}}
 ;
   )
