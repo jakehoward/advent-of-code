94dc342c62b4a9dec97452320556d1352e015a1e
Jake Howard <jake@jakehowrd.org>
2023-12-28 15:33:34 +0000

    Plugging away at day 17
    - starting to wonder if this problem is solveable for large general datasets or if I should start looking at/for specific patterns in the data. Seems like worst case of A* like algo is nearly eqivalent to brute force

--- 2023/src/aoc/day_17.clj
+++ 2023/src/aoc/day_17.clj
@@ -34,7 +34,7 @@
 ;; Attempt to make that that requires re-visiting node to get cheapest path
 ;; - Not 100% convinced, but it seems it might not be possible to force a loop.
 ;;   Would it be (easier?) with larger numbers or a different path max run-length
-(def pathalogical-example (str/trim "
+(def pathological-example (str/trim "
 11111
 99191
 99191
@@ -136,9 +136,107 @@
            (into completed-paths (filterv #(= end-yx (-> (:yxs %) last)) new-paths))
            (inc steps)))))))
 
+(defn first-comp [a b]
+  (let [c (comp (first a) (first b))]
+    (if (not= 0 c)
+      c
+      (comp (rest a) (rest b)))))
+
+(defn neighbours [routes yx]
+  (u/get-neighbours-coords-yx routes yx {:diagonals false}))
+
+(defn best-case-cost-to-end [size y x]
+  (- (+ size size) y x 2))
+
+(defn path-cost [node-cost cheapest-nbr-cost]
+  (+ node-cost cheapest-nbr-cost))
+
+(defn best-total-cost [the-path-cost size [y x]]
+  (+ the-path-cost
+     (best-case-cost-to-end size y x)))
+
+(defn get-neighbours [all-paths path]
+  ;; get the neighbours from the routes, not the yxs
+  ;; Routes Vec, of Vec of Vec of routes (y, x, run-length/direction options)
+  ;; Valid nbr is one where yx is an extension of an existing route
+  ;; that does not move more than 3 steps in a single direction
+
+  ;; Get up/down/left/right yx from point
+  ;; If a neighbour only has a path such that yx would be the 4th in a given direction
+  ;; then it's not a valid nbr
+
+  ;; If there is no route for a given yx return it with dir and num-in-row of 1
+
+  ;; Special case for yx = start ([0 0]) ?
+  )
+
+;; Route: cost, direction, num-in-row, yxs
+
+(defn- same-direction-and-run-len? [path-a path-b]
+  (and (= (:direction path-a) (:direction path-b))
+       (= (:run path-a) (:run path-b))))
+
+(defn a-star [start-yx cell-costs]
+  (let [size       (count cell-costs)
+        first-path {:cost 0 :direction :right :run 0 :yxs [start-yx]}
+        max-steps 1500]
+    (loop [steps 0
+           all-paths (vec (repeat size (vec (repeat size nil))))
+           work-todo (sorted-set-by first-comp [0 first-path])]
+
+      (when (> steps max-steps)
+        (throw (Exception. (str "Max step count exceeded. "
+                                (count work-todo) " work-items "
+                                (count (filter identity (flatten all-paths))) " ends reached."))))
+
+      ;; todo: is it safe to end when reached cell? If you get there is it inherently
+      ;;       the cheapest way to do so? Could another work item usurp it?
+      (if (empty? work-todo) ;;(or (empty? work-todo) (not (nil? (peek (peek routes)))))
+
+        ;; TODO: maintain multiple routes (min-by :cost ???)
+        {:route (peek (peek all-paths)) :steps steps}
+
+        (let [path-to-explore (first work-todo)
+              yx              (last (:yxs path-to-explore))
+              rest-work-todo  (disj work-todo path-to-explore)
+              nbrs            (get-neighbours all-paths path-to-explore)
+              ;; TODO: somehow need to ensure that cheapest nbrs for other directions and
+              ;;       run lengths are still explored. Does this happen naturally?
+              ;;       Choosing this cheapest nbr basically shuts down future options
+              ;;       which may be cheaper. But without choosing one like this, there's
+              ;;       no point to this algorithm, it might as well be brute force...
+              cheapest-nbr    (min-by :cost nbrs)
+              newcost         (path-cost (get-in cell-costs yx) (:cost cheapest-nbr 0))
+              oldcost         (:cost path-to-explore)
+              new-run         (if (= (:direction cheapest-nbr) (:direction path-to-explore))
+                                (inc (:run path-to-explore))
+                                1)]
+          ;; TODO: is this logic still valid? Yes: there's a cheaper way to get to this exact point
+          ;;       ONLY true if we consider this point, direction and run length
+          (if (and oldcost (>= newcost oldcost))
+            (recur (inc steps) all-paths rest-work-todo)
+            (recur (inc steps)
+                   ;; TODO: maintain multiple routes
+                   (assoc-in all-paths yx
+                             (as-> (get-in all-paths yx) $
+                               ;; how much work is it saving if we still maintain all
+                               ;; other run lengths and directions? Or is it about ensuring
+                               ;; we maintain valid options?
+                               (filterv #(not (same-direction-and-run-len? path-to-explore %)) $)
+                               (conj $ {:cost      newcost
+                                        :direction (:direction path-to-explore)
+                                        :run       new-run
+                                        :yxs       (conj (:yxs cheapest-nbr []) yx)})))
+                   (into rest-work-todo
+                         (map (fn [nbr]
+                                [(best-total-cost newcost size (-> nbr :yxs last))
+                                 (-> nbr :yxs last)])
+                              nbrs)))))))))
+
+
 (defn pt1 [input]
   (let [matrix   (parse-input input)
-        cheapest (cheapest-path matrix)
+        cheapest (a-star [0 0] matrix)
         ans      (select-keys cheapest [:cost :yxs])]
     ans))
 
@@ -148,7 +246,7 @@
     ans))
 
 (comment
-  (time (pt1 pathalogical-example))
+  (time (pt1 pathological-example))
   (time (pt1 small-example))
   (time (pt1 example))
   ;; Attempt to brute force example data: 50k steps, 71,921 paths, none completed ~ 15 seconds
