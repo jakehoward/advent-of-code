e3c288e4933f39656eff27305e2e074dbdc5372d
Jake Howard <jake@jakehowrd.org>
2023-12-28 07:22:36 +0000

    Day 17 - brute force solution implemented
    - doesn't even work on the example data

--- 2023/src/aoc/day_17.clj
+++ 2023/src/aoc/day_17.clj
@@ -1,7 +1,13 @@
 (ns aoc.day-17
   (:require [aoc.utils :as u]
             [aoc.vis :as vis]
+            [flatland.ordered.set :refer [ordered-set]]
             [clojure.string :as str]))
+
+;; Assumptions:
+;; - The data doesn't/can't be set up in such a way that the cheapest path
+;;   requires re-visiting a node (i.e. in a loop to avoid high cost nodes
+;;   but to allow the constraint of only 3 moves in one direction)
 
 (def example (str/trim "
 2413432311323
@@ -17,10 +23,44 @@
 1224686865563
 2546548887735
 4322674655533"))
+
+(def small-example (str/trim "
+2413
+9215
+3215
+3214
+4322"))
+
+;; Attempt to make that that requires re-visiting node to get cheapest path
+;; - Not 100% convinced, but it seems it might not be possible to force a loop.
+;;   Would it be (easier?) with larger numbers or a different path max run-length
+(def pathalogical-example (str/trim "
+11111
+99191
+99191
+99111
+9992"))
+
 (def input (u/get-input 17))
 
 (defn parse-input [input]
   (mapv #(mapv u/parse-int %) (u/input->matrix input)))
+
+(comment
+  "
+2>>34^>>>1323
+32v>>>35v5623
+32552456v>>54
+3446585845v52
+4546657867v>6
+14385987984v4
+44578769877v6
+36378779796v>
+465496798688v
+456467998645v
+12246868655<v
+25465488877v5
+43226746555v>")
 
 (defn min-by [f coll]
   (when (seq coll)
@@ -30,138 +70,91 @@
                 min))
             coll)))
 
-(defn neighbours [size yx]
-  (let [deltas [[-1 0] [1 0] [0 -1] [0 1]]]
-    (->> deltas
-         (map #(vec (map + yx %)))
-         (filter (fn [new-yx] (every? #(< -1 % size) new-yx))))))
+(defn- new-direction [old-direction yx nbr-yx]
+  (cond (u/right? yx nbr-yx) :right
+        (u/left? yx nbr-yx) :left
+        (u/above? yx nbr-yx) :up
+        (u/below? yx nbr-yx) :down
+        :else (throw (Exception.
+                      (str "Unexpected new direction " old-direction
+                           " yx: " yx
+                           " nbr-yx: " nbr-yx)))))
 
-(defn joy-heuristic [step-cost-est size y x]
-  (* step-cost-est
-     (- (+ size size) y x 2)))
+(defn- get-nbrs [matrix {:keys [direction yxs num-in-row] :as path}]
+  (let [yx             (last yxs)
+        poss-nbr-yxs   (u/get-neighbours-coords-yx matrix yx {:diagonals false})
+        straight-on    (case direction
+                         :right (u/right yx)
+                         :left  (u/left yx)
+                         :up    (u/above yx)
+                         :down  (u/below yx))]
+    (->> poss-nbr-yxs
+         (keep (fn [nbr-yx]
+                 ;; technically want to allow revisits but not
+                 ;; infinite loops
+                 (cond (contains? yxs nbr-yx)
+                       nil
 
-;; estimate-cost => h(...) in the literature (presumably "heuristic")
-(def estimate-cost joy-heuristic)
+                       (= straight-on nbr-yx)
+                       (when (< num-in-row 3)
+                         {:direction direction :num-in-row (inc num-in-row) :yx nbr-yx})
 
-;; path-cost => g(...) in the literature (presumably "god, it's annoying when ppl use symbols")
-(defn path-cost [node-cost cheapest-nbr cost-to-move]
-  (+ node-cost cost-to-move (* cost-to-move (count (:xys cheapest-nbr [])))
-     (or (:cost cheapest-nbr) 0)))
+                       :else
+                       {:direction (new-direction direction yx nbr-yx) :num-in-row 1 :yx nbr-yx})))
+         vec)))
 
-;; total-cost => f(...) in the literature
-(defn total-cost [newcost step-cost-est size [y x]]
-  (+ newcost
-     (estimate-cost step-cost-est size y x)))
+(defn cheapest-path [matrix]
+  (let [start-yx [0 0]
+        end-yx   [(dec (u/y-size matrix)) (dec (u/x-size matrix))]
+        max-steps 50000]
+    (loop [paths           [{:cost 0 :num-in-row 0 :direction :right :yxs (ordered-set start-yx)}]
+           completed-paths []
+           steps           0]
 
-(comment
-  (-> (sorted-set [0 0 [0 0]])
-      (conj [0 1 [1 1]])
-      (conj [9 -2 [9 9]])
-      (conj [1 -1 [1 1]])
-      (conj [9 -3 [9 9]])))
+      ;; #dbg ^{:break/when (= 0 (mod steps 1000))}
 
-(defn a-star [start-yx step-est cell-costs cost-to-move]
-  (let [size (count cell-costs)
-        progression (atom [])]
-    (loop [steps 0
-           routes (vec (repeat size (vec (repeat size nil))))
-           ;;                    [cost LIFO-order yx]
-           work-todo (sorted-set [0 0 start-yx])]
-      (swap! progression (fn [c] (conj c routes)))
-      (if (or (empty? work-todo) (not (nil? (peek (peek routes)))))
-        {:route (peek (peek routes)) :steps steps :progression @progression}
-        (let [[_ _ yx :as work-item] (first work-todo)
-              rest-work-todo (disj work-todo work-item)
-              nbr-yxs (neighbours size yx)
-              cheapest-nbr (min-by :cost (keep #(get-in routes %) nbr-yxs))
-              newcost (path-cost (get-in cell-costs yx) cheapest-nbr cost-to-move)
-              oldcost (:cost (get-in routes yx))]
-          (if (and oldcost (>= newcost oldcost))
-            (recur (inc steps) routes rest-work-todo)
-            (recur (inc steps)
-                   (assoc-in routes yx {:cost newcost :yxs (conj (:yxs cheapest-nbr []) yx)})
-                   (into rest-work-todo
-                         (map (fn [w] [(total-cost newcost step-est size w)
-                                       ;; ensure LIFO behaviour when cost is equal
-                                       (if (seq rest-work-todo)
-                                         (->> rest-work-todo (map second) (apply min) dec)
-                                         0)
-                                       w]) nbr-yxs)))))))))
+      (when (> steps max-steps)
+        (throw (Exception. (str "Max step count exceeded. "
+                                (count paths) " paths, "
+                                (count completed-paths) " completed paths."))))
 
+      (if (empty? paths)
+        (min-by :cost completed-paths)
+
+        (let [path      (first paths)
+              nbrs      (get-nbrs matrix path)
+              new-paths (->> nbrs
+                             (mapv (fn [nbr]
+                                     (-> path
+                                         (assoc :num-in-row (:num-in-row nbr))
+                                         (assoc :cost (+ (:cost path) (get-in matrix (:yx nbr))))
+                                         (assoc :direction (:direction nbr))
+                                         (assoc :yxs (conj (:yxs path) (:yx nbr)))))))
+              next-paths (filterv #(not= end-yx (-> (:yxs %) last)) new-paths)]
+          (recur
+           (into (vec (rest paths)) next-paths)
+           (into completed-paths (filterv #(= end-yx (-> (:yxs %) last)) new-paths))
+           (inc steps)))))))
 
 (defn pt1 [input]
-  (let [matrix  (parse-input input)
-        ans     (a-star [0 0] 1 matrix 1)]
+  (let [matrix   (parse-input input)
+        cheapest (cheapest-path matrix)
+        ans      (select-keys cheapest [:cost :yxs])]
     ans))
 
 (defn pt2 [input]
-  (let [parsed (parse-input input)
-        ans    parsed]
+  (let [matrix (parse-input input)
+        ans    matrix]
     ans))
 
 (comment
-  (count (:progression (pt1 example)))
+  (time (pt1 pathalogical-example))
+  (time (pt1 small-example))
+  (time (pt1 example))
+  ;; Attempt to brute force example data: 50k steps, 71,921 paths, none completed ~ 15 seconds
+
   (pt1 input)
   (pt2 example)
   (pt2 input)
-
-  (let [matrix [[1  99 99 99 99 99 99 99 99]
-                [1  66 66 66 66 66 66 66 66]
-                [1  1  1  1  1  1  1  1  1 ]
-                [66 66 66 66 66 66 66 66 1 ]
-                [99 99 99 99 99 99 99 99 1 ]
-                [1  1  1  1  1  1  1  1  1 ]
-                [1  66 66 66 66 66 66 66 66]
-                [1  99 99 99 99 99 99 99 99]
-                [1  1  1  1  1  1  1  1  1 ]]
-        {:keys [progression route]} (a-star [0 0] 1 matrix 1)
-        path-yxs-set (set (:yxs route))
-        grids-to-vis (->> progression
-                          (mapv
-                           (fn [grid]
-                             (mapv (fn [row] (mapv (fn [i] (:cost i)) row)) grid))))
-        max-cost    (apply max (filter identity (flatten grids-to-vis)))
-        scale-cost (fn [cost] (int (* 9 (/ cost max-cost))))]
-    (->> (vis/grids->html grids-to-vis
-                          (fn [m yx]
-                            {:shape :rect
-                             :text (or (str (get-in matrix yx)) "-")
-                             :color (if (get-in m yx)
-                                      (if (contains? path-yxs-set yx)
-                                        "green"
-                                        (nth vis/ten-step-palette (scale-cost (get-in m yx))))
-                                      "darkgray")}))
-         (spit "vis.html")))
-
-  (def random-matrix (vec (repeatedly 15 #(vec (repeatedly 15 (fn [] (rand-int 10)))))))
-
-  (let [matrix                (parse-input example)
-        ;; {:keys [progression]} (a-star [0 0] 1 matrix 1)
-        matrix random-matrix
-        {:keys [progression route]} (a-star [0 0] 1 matrix 1)
-        path-yxs-set (set (:yxs route))
-        grids-to-vis (->> progression
-                          (mapv
-                           (fn [grid]
-                             (mapv (fn [row] (mapv (fn [i] (:cost i)) row)) grid))))
-        max-cost    (apply max (filter identity (flatten grids-to-vis)))
-        scale-cost (fn [cost] (int (* 9 (/ cost max-cost))))]
-    (->> (vis/grids->html grids-to-vis
-                          (fn [m yx]
-                            {:shape :rect
-                             :text (or (str (get-in m yx)) "-")
-                             ;; :text (or (str (get-in matrix yx)) "-")
-                             :color (if (get-in m yx)
-                                      (if (contains? path-yxs-set yx)
-                                        "green"
-                                        (nth vis/ten-step-palette (scale-cost (get-in m yx))))
-                                      "darkgray")}))
-         (spit "vis.html")))
-
-((fn [cost] (int (* 9 (/ cost 109)))) 109)
-(->> (vis/grids->html [(parse-input example)]
-                        (fn [m yx]
-                          {:shape :rect :color (nth vis/ten-step-palette (get-in m yx))}))
-       (spit "vis.html"))
-;
-)
+  ;;
+  )
---
+++ 2023/src/aoc/vis_a_star_example.clj
@@ -0,0 +1,165 @@
+(ns aoc.vis-a-star-example
+  (:require [aoc.utils :as u]
+            [aoc.vis :as vis]
+            [clojure.string :as str]))
+
+(def example (str/trim "
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"))
+
+(defn parse-input [input]
+  (mapv #(mapv u/parse-int %) (u/input->matrix input)))
+
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other))
+                other
+                min))
+            coll)))
+
+(defn neighbours [routes yx]
+  (u/get-neighbours-coords-yx routes yx {:diagonals false}))
+
+(defn joy-heuristic [step-cost-est size y x]
+  (* step-cost-est
+     (- (+ size size) y x 2)))
+
+(def estimate-cost joy-heuristic)
+
+(defn path-cost [node-cost cheapest-nbr cost-to-move]
+  (+ node-cost cost-to-move (* cost-to-move (count (:xys cheapest-nbr [])))
+     (or (:cost cheapest-nbr) 0)))
+
+(defn total-cost [newcost step-cost-est size [y x]]
+  (+ newcost
+     (estimate-cost step-cost-est size y x)))
+
+
+(defn a-star [start-yx step-est cell-costs cost-to-move]
+  (let [size (count cell-costs)
+        progression (atom [])]
+    (loop [steps 0
+           routes (vec (repeat size (vec (repeat size nil))))
+           ;;                    [cost LIFO-order yx]
+           work-todo (sorted-set [0 0 start-yx])]
+      (swap! progression (fn [c] (conj c routes))) ;; -> async channel
+      (if (or (empty? work-todo) (not (nil? (peek (peek routes)))))
+        {:route (peek (peek routes)) :steps steps :progression @progression}
+        (let [[_ _ yx :as work-item] (first work-todo)
+              rest-work-todo (disj work-todo work-item)
+              nbr-yxs        (neighbours routes yx)
+              cheapest-nbr   (min-by :cost (keep #(get-in routes %) nbr-yxs))
+              newcost        (path-cost (get-in cell-costs yx) cheapest-nbr cost-to-move)
+              oldcost        (:cost (get-in routes yx))]
+          (if (and oldcost (>= newcost oldcost))
+            (recur (inc steps) routes rest-work-todo)
+            (recur (inc steps)
+                   (assoc-in routes yx {:cost newcost :yxs (conj (:yxs cheapest-nbr []) yx)})
+                   (into rest-work-todo
+                         (map (fn [w] [(total-cost newcost step-est size w)
+                                       ;; ensure LIFO behaviour when cost is equal
+                                       (if (seq rest-work-todo)
+                                         (->> rest-work-todo (map second) (apply min) dec)
+                                         0)
+                                       w]) nbr-yxs)))))))))
+
+
+(defn pt1 [input]
+  (let [matrix  (parse-input input)
+        ans     (a-star [0 0] 1 matrix 1)]
+    ans))
+
+(comment
+  (let [matrix                      (parse-input example)
+        {:keys [progression route]} (a-star [0 0] 1 matrix 1)
+        path-yxs-set (set (:yxs route))
+        grids-to-vis (->> progression
+                          (mapv
+                           (fn [grid]
+                             (mapv (fn [row] (mapv (fn [i] (:cost i)) row)) grid))))
+        max-cost    (apply max (filter identity (flatten grids-to-vis)))
+        scale-cost (fn [cost] (int (* 9 (/ cost max-cost))))]
+    (->> (vis/grids->html grids-to-vis
+                          (fn [m yx]
+                            {:shape :rect
+                             :text (or (str (get-in m yx)) "-")
+                             ;; :text (or (str (get-in matrix yx)) "-")
+                             :color (if (get-in m yx)
+                                      (if (contains? path-yxs-set yx)
+                                        "green"
+                                        (nth vis/ten-step-palette (scale-cost (get-in m yx))))
+                                      "darkgray")}))
+         (spit "vis.html")))
+
+  (let [matrix [[1  99 99 99 99 99 99 99 99]
+                [1  66 66 66 66 66 66 66 66]
+                [1  1  1  1  1  1  1  1  1 ]
+                [66 66 66 66 66 66 66 66 1 ]
+                [99 99 99 99 99 99 99 99 1 ]
+                [1  1  1  1  1  1  1  1  1 ]
+                [1  66 66 66 66 66 66 66 66]
+                [1  99 99 99 99 99 99 99 99]
+                [1  1  1  1  1  1  1  1  1 ]]
+        {:keys [progression route]} (a-star [0 0] 1 matrix 1)
+        path-yxs-set (set (:yxs route))
+        grids-to-vis (->> progression
+                          (mapv
+                           (fn [grid]
+                             (mapv (fn [row] (mapv (fn [i] (:cost i)) row)) grid))))
+        max-cost    (apply max (filter identity (flatten grids-to-vis)))
+        scale-cost (fn [cost] (int (* 9 (/ cost max-cost))))]
+    (->> (vis/grids->html grids-to-vis
+                          (fn [m yx]
+                            {:shape :rect
+                             :text (or (str (get-in matrix yx)) "-")
+                             :color (if (get-in m yx)
+                                      (if (contains? path-yxs-set yx)
+                                        "green"
+                                        (nth vis/ten-step-palette (scale-cost (get-in m yx))))
+                                      "darkgray")}))
+         (spit "vis.html")))
+
+  (def random-matrix (vec (repeatedly 15 #(vec (repeatedly 15 (fn [] (rand-int 10)))))))
+
+  (let [matrix                (parse-input example)
+        ;; {:keys [progression]} (a-star [0 0] 1 matrix 1)
+        matrix random-matrix
+        {:keys [progression route]} (a-star [0 0] 1 matrix 1)
+        path-yxs-set (set (:yxs route))
+        grids-to-vis (->> progression
+                          (mapv
+                           (fn [grid]
+                             (mapv (fn [row] (mapv (fn [i] (:cost i)) row)) grid))))
+        max-cost    (apply max (filter identity (flatten grids-to-vis)))
+        scale-cost (fn [cost] (int (* 9 (/ cost max-cost))))]
+    (->> (vis/grids->html grids-to-vis
+                          (fn [m yx]
+                            {:shape :rect
+                             :text (or (str (get-in m yx)) "-")
+                             ;; :text (or (str (get-in matrix yx)) "-")
+                             :color (if (get-in m yx)
+                                      (if (contains? path-yxs-set yx)
+                                        "green"
+                                        (nth vis/ten-step-palette (scale-cost (get-in m yx))))
+                                      "darkgray")}))
+         (spit "vis.html")))
+
+((fn [cost] (int (* 9 (/ cost 109)))) 109)
+(->> (vis/grids->html [(parse-input example)]
+                        (fn [m yx]
+                          {:shape :rect :color (nth vis/ten-step-palette (get-in m yx))}))
+       (spit "vis.html"))
+;
+)
