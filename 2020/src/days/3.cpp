0439b11096ae721c31014f6010c4d75dd8331eab
Jake Howard <jake@jakehoawrd.org>
2024-09-12 20:58:27 +0100

    2020: day 3 pt 2

--- 2020/src/days/3.cpp
+++ 2020/src/days/3.cpp
@@ -15,14 +15,9 @@ namespace Day3 {
 #...##....#
 .#..#...#.#)";
 
-    void part_ii(std::string const &input) {
-        std::cout << "The answer is: " << "TBD!" << std::endl;
-    }
-
-    void part_i(std::string const &input) {
-        auto m = Utils::buildMatrix(input, {true});
+    long count_trees(Matrix<char, long> const & m, int dx, int dy) {
         int latch = 0;
-        int tree_count = 0;
+        long tree_count = 0;
         int x = 0;
         int y = 0;
         while(latch < 10'000) {
@@ -30,12 +25,29 @@ namespace Day3 {
             if (m.at(x, y) == '#') {
                 ++tree_count;
             }
-            x += 3;
-            y += 1;
+            y += dy;
+            x += dx;
             if (!m.inBounds(x, y)) {
                 break;
             }
         }
+        return tree_count;
+    }
+
+    void part_ii(std::string const &input) {
+        auto m = Utils::buildMatrix(input, {true});
+        auto a = count_trees(m, 1, 1); // Right 1, down 1.
+        auto b = count_trees(m, 3, 1); // Right 3, down 1. (This is the slope you already checked.)
+        auto c = count_trees(m, 5, 1); // Right 5, down 1.
+        auto d = count_trees(m, 7, 1); // Right 7, down 1.
+        auto e = count_trees(m, 1, 2); // Right 1, down 2.
+        auto ans = a * b * c * d * e;
+        std::cout << "The answer is: " << ans << std::endl;
+    }
+
+    void part_i(std::string const &input) {
+        auto m = Utils::buildMatrix(input, {true});
+        auto tree_count = count_trees(m, 3, 1);
         std::cout << "The answer is: " << tree_count << std::endl;
     }
 }
