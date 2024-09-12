83a713de7d41df84583b768e3e53ca0c0dabc717
Jake Howard <jake@jakehoawrd.org>
2024-09-12 16:36:11 +0100

    2020: add timing

--- 2020/main.cpp
+++ 2020/main.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 #include "src/days/index.h"
 #include "utils.h"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
     if (argc > 2) {
         auto mode = Utils::parseMode(std::string(argv[2]));
+        auto start = std::chrono::high_resolution_clock::now();
+
         if (std::string(argv[1]) == "1") {
             Days::run1(mode);
         } else {
             throw std::runtime_error("Cannot find function for: " + std::string(argv[1]));
         }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        std::cout << "Time taken: " << diff.count() << " µs" << std::endl;
     } else {
         std::cerr << "Usuage: ./aoc <day-to-run: 1-25> <mode: example|input>" << std::endl;
     }
