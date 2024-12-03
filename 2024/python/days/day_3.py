541a861de78ee2ddd41ad52e371387c619b849eb
Jake Howard <jake@jakehoawrd.org>
2024-12-03 13:10:10 +0000

    Python Day 3 much slower using numpy (naively)

--- 2024/python/days/day_3.py
+++ 2024/python/days/day_3.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from utils.misc import timer
 from utils.read import read_input
 import re
+import numpy as np
 
 example = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
 example_2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
@@ -29,27 +30,33 @@ def part2(input):
         i += 1
     print(f'Pt2::ans: {answer}')
 
-def part2_fast(input):
+def part2_fast(input_str):
+    input = np.array(list(input_str))
     pos = 0
     on = True
     input_len = len(input)
     answer = 0
+    do_array = np.array(list("do()"))
+    dont_array = np.array(list("don't()"))
+    mul_array = np.array(list("mul("))
     while pos < input_len:
-        if input[pos:pos +4] == "do()":
+        if input[pos:pos +4].size == 4 and np.all(input[pos:pos + 4] == do_array):
             on = True
             pos += 4
-        elif input[pos:pos + 7] == "don't()":
+        elif input[pos: pos + 7].size == 7 and np.all(input[pos:pos + 7] == dont_array):
             on = False
             pos += 7
         elif on:
-            if input[pos:].startswith("mul("):
+            if input[pos: pos + 4].size == 4 and np.all(input[pos:pos + 4] == mul_array):
                 pos += 4
-                next_comma_idx = input[pos:].find(',')
-                next_paren_idx = input[pos:].find(')')
+                comma_matches = np.where(input[pos:] == ',')
+                next_comma_idx =  comma_matches[0][0] if len(comma_matches[0]) else -1
+                paren_matches = np.where(input[pos:] == ')')
+                next_paren_idx = paren_matches[0][0] if len(paren_matches[0]) else -1
                 mul_len = next_paren_idx
                 if next_comma_idx != -1 and next_paren_idx != -1 and next_comma_idx >= 1 and next_paren_idx > next_comma_idx and 3 <= mul_len and mul_len <= 7:
-                    first = input[pos:pos + next_comma_idx]
-                    second = input[pos + next_comma_idx + 1: pos + next_paren_idx]
+                    first = ''.join(input[pos:pos + next_comma_idx])
+                    second = ''.join(input[pos + next_comma_idx + 1: pos + next_paren_idx])
                     if first.isdigit() and second.isdigit():
                         answer += int(first) * int(second)
                         pos += mul_len
@@ -57,7 +64,7 @@ def part2_fast(input):
                 pos += 1
         else:
             pos += 1
-    print(f'Pt2::ans: {answer}')
+    print(f'Pt2_fast::ans: {answer}')
 
 def run():
     day = Path(__file__).name.split('.')[0].split('_')[-1]
