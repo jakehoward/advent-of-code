4c9678af5199d13657726edede0a16cb5600ecd8
Jake Howard <jake@jakehoawrd.org>
2024-12-10 06:17:31 +0000

    2024 Day 10 (Python)

---
+++ 2024/python/days/day_10.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+
+from utils.grid import make_grid
+from utils.misc import timer
+from utils.read import read_input
+
+example = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+
+small_example = """
+0123
+1234
+8765
+9876""".strip()
+
+def part1(input):
+    grid = make_grid(input, as_ints=True)
+    trailheads = []
+    for y in range(grid._y_size):
+        for x in range(grid._x_size):
+            if grid.at(x, y) == 0:
+                trailheads.append((x, y))
+
+    point_to_tops_reached = {}
+    def iter(point):
+        if point in point_to_tops_reached:
+            return point_to_tops_reached[point]
+
+        val = grid.at_p(point)
+        if val == 9:
+            return set([point])
+        nbr_xys = grid.get_nbr_xys(point[0], point[1])
+        tops_reached = set()
+        for nbr_point in nbr_xys:
+            if grid.at_p(nbr_point) == val + 1:
+                tops_reached = tops_reached.union(iter(nbr_point))
+        point_to_tops_reached[point] = tops_reached
+        return tops_reached
+
+    answer = 0
+    for trailhead in trailheads:
+        answer += len(iter(trailhead))
+
+    return answer
+
+def part2(input):
+    grid = make_grid(input, as_ints=True)
+    trailheads = []
+    for y in range(grid._y_size):
+        for x in range(grid._x_size):
+            if grid.at(x, y) == 0:
+                trailheads.append((x, y))
+
+    point_to_num_routes = {}
+    def iter(point):
+        if point in point_to_num_routes:
+            return point_to_num_routes[point]
+
+        val = grid.at_p(point)
+        if val == 9:
+            return 1
+        nbr_xys = grid.get_nbr_xys(point[0], point[1])
+        num_routes = 0
+        for nbr_point in nbr_xys:
+            if grid.at_p(nbr_point) == val + 1:
+                num_routes += iter(nbr_point)
+        point_to_num_routes[point] = num_routes
+        return num_routes
+
+    answer = 0
+    for trailhead in trailheads:
+        answer += iter(trailhead)
+
+    return answer
+
+def run():
+    day = Path(__file__).name.split('.')[0].split('_')[-1]
+    input = read_input(day)
+    with timer():
+        ans = part1(small_example)
+        assert ans == 1, "Got: {}".format(ans)
+        print(f'Pt1(small_example)::ans: {ans}')
+
+    with timer():
+        ans = part1(example)
+        assert ans == 36, "Got: {}".format(ans)
+        print(f'Pt1(example)::ans: {ans}')
+
+    with timer():
+        ans = part1(input)
+        assert ans == 482, "Got: {}".format(ans)
+        print(f'Pt1::ans: {ans}')
+
+    with timer():
+        ans = part2(example)
+        assert ans == 81, "Got: {}".format(ans)
+        print(f'Pt2_v2(example)::ans: {ans}')
+
+    with timer():
+        ans = part2(input)
+        assert ans == 1094, "Got: {}".format(ans)
+        print(f'Pt2::ans: {ans}')
+
+
+if __name__ == "__main__":
+    run()
