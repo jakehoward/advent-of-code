ad999878528d9894da7ea2300e6f6a90f96e3efd
Jake Howard <jake@jakehoawrd.org>
2024-12-12 16:23:38 +0000

    2024 python - Clean up Day 11

--- 2024/python/days/day_11.py
+++ 2024/python/days/day_11.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import cache
 
 from utils.misc import timer
 from utils.read import read_input
@@ -15,48 +16,20 @@ def calc_next_stones(stone):
     return [stone * 2024]
 
 
-def brute_force(start_stone, n_times, stone_iters_to_ans=None):
-    if stone_iters_to_ans and stone_iters_to_ans.get((start_stone, n_times)):
-        return stone_iters_to_ans[(start_stone, n_times)]
-
-    stones = [start_stone]
-    for i in range(n_times):
-        next_stones = []
-        for stone in stones:
-            next_stones += calc_next_stones(stone)
-        if stone_iters_to_ans: stone_iters_to_ans[(start_stone, i + 1)] = len(next_stones)
-        stones = next_stones
-    answer = len(stones)
-    return answer
+@cache
+def solve_for_stone(start_stone, iters):
+    if iters == 0:
+        return 1
+    ans = sum([solve_for_stone(stone, iters - 1) for stone in calc_next_stones(start_stone)])
+    return ans
 
 
 def part1(input):
-    stones = [int(stone) for stone in input.split()]
-    ans = 0
-    for stone in stones:
-        ans += brute_force(stone, 25)
-    return ans
+    return sum([solve_for_stone(int(stone), 25) for stone in input.split()])
 
 
-def solve_for_stone(start_stone, iters, stone_iters_to_ans):
-    if known_ans := stone_iters_to_ans.get((start_stone, iters)):
-        return known_ans
-    if iters == 0:
-        return 1
-    ans = sum([solve_for_stone(stone, iters - 1, stone_iters_to_ans) for stone in calc_next_stones(start_stone)])
-    stone_iters_to_ans[(start_stone, iters)] = ans
-    return ans
-
-
-def part2(input, n_times):
-    stones = [int(stone) for stone in input.split()]
-    stone_to_num_iters_to_ans = {}
-    ans = 0
-    for i, stone in enumerate(stones):
-        print(f'stone {i + 1} of {len(stones)}')
-        num_stones = solve_for_stone(stone, n_times, stone_to_num_iters_to_ans)
-        ans += num_stones
-    return ans
+def part2(input):
+    return sum([solve_for_stone(int(stone), 75) for stone in input.split()])
 
 
 def run():
@@ -73,7 +46,7 @@ def run():
         print(f'Pt1::ans: {ans}')
 
     with timer():
-        ans = part2(input, 75)
+        ans = part2(input)
         assert ans == 238317474993392, "Got: {}".format(ans)
         print(f'Pt2::ans: {ans}')
 
