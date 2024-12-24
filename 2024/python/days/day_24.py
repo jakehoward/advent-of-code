8d4eacf259d738ae9acdb97dc65247c5f8194bec
Jake Howard <jake@jakehoawrd.org>
2024-12-24 08:45:49 +0000

    Day 24 pt 2 WIP

--- 2024/python/days/day_24.py
+++ 2024/python/days/day_24.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from itertools import combinations
 from pathlib import Path
 
@@ -57,22 +58,22 @@ tnw OR pbm -> gnj""".strip()
 def parse(input):
     initial_chunk, graph_chunk = input.split('\n\n')
     initial = [(name, bool(int(value))) for name, value in [line.split(': ') for line in initial_chunk.splitlines()]]
-    graph = []
+    circuit = []
     for line in graph_chunk.splitlines():
         input, out = line.split(' -> ')
         a, op, b = input.split()
-        graph.append(((a, op, b), out))
-    return initial, graph
+        circuit.append(((a, op, b), out))
+    return initial, tuple(circuit)
 
 
-def part1(input):
-    initial, graph = parse(input)
-    wire_values = {name: value for name, value in initial}
-    remaining = graph
-    while remaining:
-        for _ in range(len(remaining)):
-            conn = remaining.pop(0)
-            (a, op, b), out = conn
+def run_machine(wire_value_list, _circuit):
+    circuit = list(_circuit)
+    wire_values = {name: value for name, value in wire_value_list}
+    remaining_gates = circuit
+    while remaining_gates:
+        for _ in range(len(remaining_gates)):
+            gate = remaining_gates.pop(0)
+            (a, op, b), out = gate
             if a in wire_values and b in wire_values:
                 if op == 'AND':
                     wire_values[out] = wire_values[a] and wire_values[b]
@@ -81,28 +82,120 @@ def part1(input):
                 elif op == 'XOR':
                     wire_values[out] = wire_values[a] ^ wire_values[b]
             else:
-                remaining.append(conn)
+                remaining_gates.append(gate)
 
     z_name_val_pairs = sorted([(name, int(value)) for name, value in wire_values.items() if name.startswith('z')],
                               key=lambda x: x[0], reverse=True)
     binary_string = ''.join([str(val) for name, val in z_name_val_pairs])
-    # print(z_name_val_pairs, binary_string)
-    answer = int(binary_string, 2)
-    return answer
+    return int(binary_string, 2)
 
+
+def part1(input):
+    wire_value_list, circuit = parse(input)
+    return run_machine(wire_value_list, circuit)
+
+
+def get_name_val_pairs(wire_value_list, n):
+    nv = [(name, int(value)) for name, value in wire_value_list if name.startswith(n)]
+    return sorted(nv, key=lambda x: x[0], reverse=True)
+
+
+def get_number_from_input(wire_value_list, n):
+    assert n == 'x' or n == 'y' or n == 'z'
+    name_val_pairs = get_name_val_pairs(wire_value_list, n)
+    binary_string = ''.join([str(val) for name, val in name_val_pairs])
+    return int(binary_string, 2)
+
+                          # in op in`  -> out
+type RawGate = tuple[tuple[str, str, str], str]
+type RawCircuit = tuple[RawGate]
+
+Z_Output = namedtuple('Z_Output', ['name', 'number'])
+Input = namedtuple('Input', ['name'])
+Output = namedtuple('Output', ['name'])
+Carry = namedtuple('Carry', ['name', 'to'])
+Gate = namedtuple('Gate', ['inputs', 'op', 'out'])
+Result = namedtuple('Result', ['swapped_gates', 'carry', 'fixed_circuit'])
+
+def get_only_gate(circuit: RawCircuit, inputs: set[Input], op: str) -> Gate:
+    # Get matching gate. Raise if no gate or multiple gates
+    pass
+
+
+def maybe_get_only_gate(circuit: RawCircuit, inputs: set[Input], op: str) -> Gate:
+    # Get single matching gate or None, Raise if multiple gates
+    pass
+
+
+def trace_full_adder(initial_circuit: RawCircuit, x_in: Input, y_in: Input, carry: Carry) -> Result:
+    circuit = initial_circuit
+    swapped_gates = []
+
+
+    ## First and second XOR
+    xor_1 = get_only_gate(circuit, inputs={x_in, y_in}, op='XOR')
+    xor_2 = maybe_get_only_gate(circuit, inputs={xor_1.out, carry.name}, op='XOR')
+    # if xor_2 is None -> xor_1 has the wrong output. Find output that goes into xor with carry and swap
+    # if xor_2.out is not Z[input_n], xor_2 has wrong output, find gate that outputs to z[input_n] and swap
+
+    # Update fixed values
+    xor_1 = get_only_gate(circuit, inputs={x_in, y_in}, op='XOR')
+    xor_2 = get_only_gate(circuit, inputs={xor_1.out, carry.name}, op='XOR')
+
+    ## First and second AND
+    and_1 = get_only_gate(circuit, inputs={xor_1.out, carry.name}, op='AND')
+    and_2 = get_only_gate(circuit, inputs={x_in, y_in}, op='AND')
+    final_or = maybe_get_only_gate(circuit, inputs={and_1.out, and_2.out}, op='OR')
+    # if not final_or and_1 or and_2, or both, have the wrong output, they should input to an OR (tricky?)
+    # if final_or.out not z45 OR the input to an XOR and an AND, wrong output, needs to be swapped
+
+    ## todo: (Final OR).out
+    out_carry = final_or.out
+
+    return Result(swapped_gates, out_carry, circuit)
+
+
+
+def find_swapped_gates(wire_value_list, initial_circuit):
+    x_wires = [name for name, _ in get_name_val_pairs(wire_value_list, 'x')]
+    y_wires = [name for name, _ in get_name_val_pairs(wire_value_list, 'y')]
+    x_wires.reverse()
+    y_wires.reverse()
+    xy_pairs = list(zip(x_wires, y_wires))
+
+    all_swapped_gates = []
+    latest_circuit = initial_circuit
+    latest_carry = Carry('gct', 1) # Manual analysis
+    for x_in, y_in in xy_pairs[1:]:
+        result = trace_full_adder(latest_circuit, Input(x_in), Input(y_in), latest_carry)
+        for a,b in result.swapped_gates:
+            all_swapped_gates += [a, b]
+        latest_carry = result.carry
+        latest_circuit = result.fixed_circuit
+
+    return ','.join(sorted(all_swapped_gates))
+
+
+def analysis(wire_value_list, circuit):
+    x = get_number_from_input(wire_value_list, n='x')
+    y = get_number_from_input(wire_value_list, n='y')
+    machine_before = run_machine(wire_value_list, circuit)
+    # print('x:', x, 'y:', y, 'x+y:', x + y, 'machine:', machine_before, 'delta:', x + y - machine_before)
+
+    print('Circuit:')
+    g = []
+    for gate in sorted(circuit):
+        (a, op, b), out = gate
+        if op == 'XOR' and (a.startswith('x') or a.startswith('y')):
+            g.append(gate)
+    print(len(g))
 
 
 def part2(input):
-    input, graph = parse(input)
-    x_name_val_pairs = sorted([(name, int(value)) for name, value in input if name.startswith('x')],
-                              key=lambda x: x[0], reverse=True)
-    print(x_name_val_pairs)
-    # wire_values = {name: value for name, value in input}
-    # outputs = [out for _, out in graph]
-    # pairs = list(combinations(outputs, 2))
-    # print('pairs:', len(pairs), 'four pairs:', len(list(combinations(pairs, 4))))
-    answer = '...'
-    return answer
+    wire_value_list, circuit = parse(input)
+
+    # analysis(wire_value_list, circuit)
+    return find_swapped_gates(wire_value_list, circuit)
 
 
 def run():
@@ -116,21 +209,21 @@ def run():
 
     with timer():
         ans = part1(input)
-        # assert ans == None, "Got: {}".format(ans)
+        assert ans == 52728619468518, "Got: {}".format(ans)
         print(f'Pt1::ans: {ans}')
         ans = None
 
-    with timer():
-        ans = part2(example)
-    #     assert ans == None, "Got: {}".format(ans)
-        print(f'Pt2(example)::ans: {ans}')
-        ans = None
-
     # with timer():
-    #     ans = part2(input)
+    #     ans = part2(example)
     #     assert ans == None, "Got: {}".format(ans)
-    #     print(f'Pt2::ans: {ans}')
+    #     print(f'Pt2(example)::ans: {ans}')
     #     ans = None
+
+    with timer():
+        ans = part2(input)
+        # assert ans == None, "Got: {}".format(ans)
+        print(f'Pt2::ans: {ans}')
+        ans = None
 
 
 if __name__ == "__main__":
