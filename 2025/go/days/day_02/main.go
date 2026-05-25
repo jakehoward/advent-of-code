c2b911ee2c66402191bda3ee1e45becd0b8ae489
Jake Howard <jake@jakehowrd.org>
2026-05-25 19:08:19 +0100

    Day 2 - 2025 golang

--- 2025/go/days/day_02/main.go
+++ 2025/go/days/day_02/main.go
@@ -12,15 +12,15 @@ import (
 const example = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
 
 type Range struct {
-	Start int
-	End   int
+	Start uint64
+	End   uint64
 }
 
 func parseRange(input string) Range {
 	startEnd := strings.Split(input, "-")
 	start, _ := strconv.Atoi(startEnd[0])
 	end, _ := strconv.Atoi(startEnd[1])
-	return Range{start, end}
+	return Range{uint64(start), uint64(end)}
 }
 
 func parseInput(input string) []Range {
@@ -34,9 +34,78 @@ func parseInput(input string) []Range {
 	return ranges
 }
 
-func ans(input string) int {
-	fmt.Println(parseInput(input))
-	return 0
+func allEqual(xs []string) bool {
+	for _, x := range xs {
+		if xs[0] != x {
+			return false
+		}
+	}
+	return true
+}
+
+func chunk(s string, size int) ([]string, bool) {
+	chunks := make([]string, len(s)/size)
+	ok := (len(s) % size) == 0
+
+	for i := 0; i < len(chunks); i++ {
+		if len(s) >= i*size+size {
+			chunks[i] = s[i*size : i*size+size]
+		}
+	}
+
+	return chunks, ok
+}
+
+func repeats(n uint64) bool {
+	ns := strconv.FormatUint(n, 10)
+
+	for i := 0; i < len(ns)/2; i++ {
+		pattern := ns[0 : i+1]
+		chunks, ok := chunk(ns, len(pattern))
+		if !ok {
+			continue
+		}
+		if allEqual(chunks) {
+			return true
+		}
+	}
+	return false
+}
+
+func repeatsTwice(n uint64) bool {
+	ns := strconv.FormatUint(n, 10)
+
+	if len(ns)%2 != 0 {
+		return false
+	}
+
+	return ns[0:len(ns)/2] == ns[len(ns)/2:]
+}
+
+func ans(input string) uint64 {
+	sumRepeats := uint64(0)
+
+	for _, r := range parseInput(input) {
+		for i := r.Start; i <= r.End; i++ {
+			if repeatsTwice(i) {
+				sumRepeats += i
+			}
+		}
+	}
+	return sumRepeats
+}
+
+func ansII(input string) uint64 {
+	sumRepeats := uint64(0)
+
+	for _, r := range parseInput(input) {
+		for i := r.Start; i <= r.End; i++ {
+			if repeats(i) {
+				sumRepeats += i
+			}
+		}
+	}
+	return sumRepeats
 }
 
 // func ansII(input string) int {
@@ -46,9 +115,11 @@ func ans(input string) int {
 func main() {
 	defer utils.PrintDuration("Day 2, main", time.Now())
 
+	fmt.Println("input", parseInput(example))
+
 	fmt.Println("ans (example):", ans(example))
 	fmt.Println("ans (file):", ans(utils.PuzzleInput("02.txt")))
 
-	// fmt.Println("ans ii (example):", ansII(example))
-	// fmt.Println("ans ii (file):", ansII(utils.PuzzleInput("02.txt")))
+	fmt.Println("ans ii (example):", ansII(example))
+	fmt.Println("ans ii (file):", ansII(utils.PuzzleInput("02.txt")))
 }
--- 2025/go/days/day_02/main_test.go
+++ 2025/go/days/day_02/main_test.go
@@ -1,1 +1,119 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestRepeats(t *testing.T) {
+	testCases := []struct {
+		name     string
+		num      uint64
+		expected bool
+	}{
+		{"A", 0, false},
+		{"B", 1, false},
+		{"C", 22, true},
+		{"C", 222, true},
+		{"D", 2121, true},
+		{"E", 21212, false},
+		{"F", 456456, true},
+		{"G", 4564567, false},
+		{"H", 45645645, false},
+		{"I", 456456456, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := repeats(tc.num)
+			if result != tc.expected {
+				t.Errorf("ex: %v, got %v; want %v", tc.num, result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestRepeatsTwice(t *testing.T) {
+	testCases := []struct {
+		name     string
+		num      uint64
+		expected bool
+	}{
+		{"A", 0, false},
+		{"B", 1, false},
+		{"C", 22, true},
+		{"D", 2222, true},
+		{"E", 2121, true},
+		{"F", 21212, false},
+		{"G", 212121, false},
+		{"H", 456456, true},
+		{"I", 456456456, false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := repeatsTwice(tc.num)
+			if result != tc.expected {
+				t.Errorf("ex: %v, got %v; want %v", tc.num, result, tc.expected)
+			}
+		})
+	}
+}
+
+func eq[T comparable](xs []T, ys []T) bool {
+	if len(xs) != len(ys) {
+		return false;
+	}
+	for i, x := range xs {
+		if x != ys[i] {
+			return false
+		}
+	}
+
+	return true;
+}
+
+func TestChunks(t *testing.T) {
+	testCases := []struct {
+		name     string
+		s        string
+		size     int
+		ok       bool
+		expected []string
+	}{
+		{"A", "abc", 1, true, []string{"a", "b", "c"}},
+		{"B", "abc", 2, false, []string{}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, ok := chunk(tc.s, tc.size)
+			if ok && tc.ok {
+				if !eq(result, tc.expected) {
+					t.Errorf("ex: %v (%v), got %v; want %v", tc.s, tc.size, result, tc.expected)
+				}
+			} else {
+				if tc.ok != ok {
+					t.Errorf("got ok %v; want ok %v", ok, tc.ok)
+				}
+			}
+		})
+	}
+}
+
+func TestPartOne(t *testing.T) {
+	got := ans(example)
+	want := uint64(1227775554)
+
+	if want != got {
+		t.Errorf("got %v; want: %v", got, want)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	got := ansII(example)
+	want := uint64(4174379265)
+
+	if want != got {
+		t.Errorf("got %v; want: %v", got, want)
+	}
+}
